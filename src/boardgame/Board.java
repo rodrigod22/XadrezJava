78cd1aa8de0da9895eae4c62679f1d6ddb747ae1
Rodrigo <rodrigogat5@hotmail.com>
2019-01-24 08:38:36 -0200

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+public class Board {
+    
+    private int rows;
+    private int colunms;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.colunms = columns;
+        pieces = new Piece[rows][colunms];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColunms() {
+        return colunms;
+    }
+
+    public void setColunms(int colunms) {
+        this.colunms = colunms;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
