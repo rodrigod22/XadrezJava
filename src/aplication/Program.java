20fbf9633ecf54568c9a2228dbc342f5227be0f5
Rodrigo <rodrigogat5@hotmail.com>
2019-01-24 08:25:33 -0200

    first class Position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,15 @@
 package aplication;
 
+import boardgame.Position;
+
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+        
     }
     
 }
